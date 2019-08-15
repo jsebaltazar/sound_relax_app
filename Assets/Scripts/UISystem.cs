996ccfc7be7b4d7a862802be58346b1037a63691
Jesse <jsebaltazar@gmail.com>
2019-08-14 20:15:48 -0600

    Added working UISystem and UIScreen scripts for page transition

--- Assets/Scripts/UISystem.cs
+++ Assets/Scripts/UISystem.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class UISystem : MonoBehaviour
 {
+    UIScreen previousPage;
+    UIScreen currentPage;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //     var pages = GetComponentsInChildren<UIScreen>();
+        UIScreen[] pages = GetComponentsInChildren<UIScreen>(true);
+        pages[0].gameObject.SetActive(true);
+        currentPage = pages[0];
+        for( int i = 1; i < pages.Length; i++)
+        {
+            pages[i].gameObject.SetActive(false);
+        }
+         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchPage(UIScreen page)
     {
-        
+        previousPage = currentPage;
+        currentPage.gameObject.SetActive(false);
+        page.gameObject.SetActive(true);
+        currentPage = page;
     }
+    public void SwitchBack(UIScreen page)
+    {
+        SwitchPage(page);
+    }
+
 }
